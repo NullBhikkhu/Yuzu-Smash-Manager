77b9c878e4643d0516f90aa5c3499e85c18018ab
NullBhikkhu <NullBhikkhu@protonmail.com>
2024-01-04 18:17:50 -0500

    Refactoring and cleaning.

--- yuzu_launcher.ps1
+++ yuzu_launcher.ps1
@@ -5,20 +5,28 @@ param(
 )
 
 # --- Variables ---
+# ---- Paths & Filesnames ----
 $yuzuPath = "$env:USERPROFILE\AppData\Roaming\yuzu"
 $yuzuArchivePath = "$yuzuPath\archives"
+$dependenciesPath = "$yuzuArchivePath\dependencies"
 $sevenZipPath = "C:\Program Files\7-Zip\7z.exe"
-$sevenZipUrl = "https://www.7-zip.org/a/7z2301-x64.exe"
 $sevenZipInstallerBasename = "7z2301-x64.exe"
-$sevenZipInstallerPath = "$yuzuArchivePath\dependencies\$sevenZipInstallerBasename"
+$sevenZipInstallerPath = "$dependenciesPath\$sevenZipInstallerBasename"
+
+# ---- URLs ----
+$sevenZipUrl = "https://www.7-zip.org/a/7z2301-x64.exe"
+$ldnAllInOneUrl = "https://cdn.discordapp.com/attachments/1121103766960226496/1188253223761485966/LDN_all_in_one_package.zip?ex=6599da0d&is=6587650d&hm=e3cea7384dad56f144346478b881e8439d16c43d94dc7964edab3b399f0a82fe&"
+$legacyDiscoveryUrl = "https://cdn.discordapp.com/attachments/890851835349446686/1191740302566891581/legacy_discovery?ex=65a689a5&is=659414a5&hm=b19cb9b22f67d4ca97da49b3b51c9242be57fb2124e74a894d072260225576fd&"
+$saveDataUrl = "https://cdn.discordapp.com/attachments/890851835349446686/1191073579727597589/save_data.rar?ex=65a41cb6&is=6591a7b6&hm=635a5a953b6c7ce64d4dd6b2dbf6b280ed29dbce5f001bafd99c6486004db965&"
+$wifiFixUrl = "https://drive.usercontent.google.com/download?id=1f_idi29L7Poxg0Cljbi4oz9ubpukmdXY&export=download"
 
 # Only static files; HDR will be acquired dynamically so it always grabs the latest release. See Get-LatestHDRReleaseUrl.
 $fileUrls = @(
-    'https://www.7-zip.org/a/7z2301-x64.exe',
-    'https://cdn.discordapp.com/attachments/1121103766960226496/1188253223761485966/LDN_all_in_one_package.zip?ex=6599da0d&is=6587650d&hm=e3cea7384dad56f144346478b881e8439d16c43d94dc7964edab3b399f0a82fe&',
-    'https://cdn.discordapp.com/attachments/890851835349446686/1191740302566891581/legacy_discovery?ex=65a689a5&is=659414a5&hm=b19cb9b22f67d4ca97da49b3b51c9242be57fb2124e74a894d072260225576fd&',
-    'https://cdn.discordapp.com/attachments/890851835349446686/1191073579727597589/save_data.rar?ex=65a41cb6&is=6591a7b6&hm=635a5a953b6c7ce64d4dd6b2dbf6b280ed29dbce5f001bafd99c6486004db965&',
-    'https://drive.usercontent.google.com/download?id=1f_idi29L7Poxg0Cljbi4oz9ubpukmdXY&export=download' # Warning! Might not be consistent.
+    $sevenZipUrl,
+    $ldnAllInOneUrl,
+    $legacyDiscoveryUrl,
+    $saveDataUrl,
+    $wifiFixUrl
 )
 
 # --- Functions ---
@@ -69,11 +77,6 @@ function Ensure-Files {
             $fileName = "Wifi-Fix (merge with current exef).zip"
         }
 
-        # # Special handling of 7-Zip installer.
-        # if ($url -eq $SevenZipInstallerName) {
-        #     Write-Host "Downloading $fileName..."
-        # }
-
         $filePath = Join-Path -Path $DownloadDir -ChildPath $fileName
 
         if (-not (Test-Path $filePath)) {
@@ -108,7 +111,7 @@ function Ensure-7zip {
 # --- Initial Setup ---
 # Create directories as necessary.
 New-Item -ItemType Directory -Path "$yuzuArchivePath" -Force | Out-Null
-New-Item -ItemType Directory -Path "$yuzuArchivePath\dependencies" -Force | Out-Null
+New-Item -ItemType Directory -Path "$dependenciesPath" -Force | Out-Null
 New-Item -ItemType Directory -Path "$yuzuArchivePath\created" -Force | Out-Null
 
 # --- Run ---
@@ -120,7 +123,7 @@ if ($latestHdrUrl) {
 }
 
 # Ensure dependencies
-Ensure-Files -Urls $fileUrls -DownloadDir "$yuzuArchivePath\dependencies"
+Ensure-Files -Urls $fileUrls -DownloadDir "$dependenciesPath"
 Ensure-7zip -SevenZipPath "$sevenZipPath" -SevenZipInstallerPath "$sevenZipInstallerPath"
 
 # ---- Switches ----
